14d3af4deb1ccf126846c77c0d9c32f983d00a68
Preston Alvarado <700740+coolhome@users.noreply.github.com>
2020-12-22 17:11:20 -0500

    0 build errors

--- src/crypto/jsrsasign.ts
+++ src/crypto/jsrsasign.ts
@@ -1,4 +1,4 @@
-import { jws, KEYUTIL as KeyUtil, X509, crypto, hextob64u, b64tohex } from '../../jsrsasign/dist/jsrsasign';
+import { jws, KEYUTIL as KeyUtil, X509, crypto, hextob64u, b64tohex } from '../../jsrsasign/dist/jsrsasign.js';
 
 const AllowedSigningAlgs = ['RS256', 'RS384', 'RS512', 'PS256', 'PS384', 'PS512', 'ES256', 'ES384', 'ES512'];
 
